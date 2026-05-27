e415b5d67cb468db1a8117879b25c8583763a830
Davin K. Tanabe <davin.tanabe@digitalasset.com>
2026-05-27 14:30:14 -0400

    feat: add support for additional postgres configuration fields
    Signed-off-by: Davin K. Tanabe <davin.tanabe@digitalasset.com>

--- core/signing-store-sql/src/schema.ts
+++ core/signing-store-sql/src/schema.ts
@@ -178,7 +178,10 @@ export const storeConfigSchema = z.object({
             type: z.literal('sqlite'),
             database: z.string(),
         }),
-        z.object({
+        // the postgres driver exposes lots of fields; add validation for some of the most
+        // important ones, but it's probably too much effort to try to keep up with all
+        // the different flags that could potentially be exposed
+        z.looseObject({
             type: z.literal('postgres'),
             host: z.string(),
             port: z.number(),
