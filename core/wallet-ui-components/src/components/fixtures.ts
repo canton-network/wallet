5b4ae25c006ad5598f207b4f03fd210b99bd52ed
Pawel Stepien <pawel.stepien@digitalasset.com>
2026-06-05 16:21:17 +0200

    networks tests
    Signed-off-by: Pawel Stepien <pawel.stepien@digitalasset.com>

--- core/wallet-ui-components/src/components/fixtures.ts
+++ core/wallet-ui-components/src/components/fixtures.ts
@@ -1,7 +1,26 @@
 // Copyright (c) 2025-2026 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import type { Idp } from '@canton-network/core-wallet-user-rpc-client'
+import type {
+    Idp,
+    PublicNetwork,
+} from '@canton-network/core-wallet-user-rpc-client'
+
+export function makePublicNetwork(
+    overrides: Partial<PublicNetwork> = {}
+): PublicNetwork {
+    return {
+        id: 'net-1',
+        name: 'Test Network',
+        description: 'Test network description',
+        authMethod: 'authorization_code',
+        synchronizerId: 'sync::id',
+        identityProviderId: 'idp-1',
+        ledgerApi: 'https://ledger.example',
+        clientId: 'client-1',
+        ...overrides,
+    }
+}
 
 export function makeIdp(overrides: Partial<Idp> = {}): Idp {
     return {
