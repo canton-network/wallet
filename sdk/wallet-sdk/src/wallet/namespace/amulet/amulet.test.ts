d9cd0ff7549c7fb70ac47aa6b827de332329e972
rukmini-basu-da <rukmini.basu@digitalasset.com>
2026-06-15 12:16:54 -0400

    test
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>

---
+++ sdk/wallet-sdk/src/wallet/namespace/amulet/amulet.test.ts
@@ -0,0 +1,75 @@
+// Copyright (c) 2025-2026 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import { mock } from '../../__test__/mocks'
+import { describe, it, vi, beforeEach } from 'vitest'
+import { AmuletNamespace, AmuletNamespaceConfig } from './namespace'
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+const { ctx } = mock
+
+const mockTokenStandard = {
+    get: vi.fn(),
+    getInputHoldingsCids: vi.fn(),
+    core: {
+        toQualifiedMemberId: vi.fn(),
+    },
+    transfer: {
+        fetchTransferFactoryChoiceContext: vi.fn(),
+    },
+}
+
+const mockAmuletService = {
+    createTap: vi.fn(),
+    selfGrantFeatureAppRight: vi.fn(),
+    getTransferPreApprovalByParty: vi.fn(),
+    getFeaturedAppsByParty: vi.fn(),
+    cancelTransferPreapproval: vi.fn(),
+    renewTransferPreapproval: vi.fn(),
+    isDevNet: vi.fn(),
+}
+const config: AmuletNamespaceConfig = {
+    commonCtx: ctx,
+    registry: {
+        id: 'Amulet',
+        displayName: 'Amulet',
+        symbol: 'CC',
+        registryUrl: new URL('http://registry.com'),
+        admin: 'adminParty:123',
+    },
+    amuletService: mockAmuletService as any,
+    tokenStandardService: mockTokenStandard as any,
+    validatorParty: 'validatorParty:123',
+}
+
+describe('amulet namespace', () => {
+    let amuletNamespace: AmuletNamespace
+
+    beforeEach(() => {
+        vi.clearAllMocks()
+        amuletNamespace = new AmuletNamespace(config)
+    })
+
+    it('should create tap', async () => {
+        mockAmuletService.createTap.mockResolvedValue({
+            templateId:
+                'a31be0483f3175647053f28965a4e6d97e3dbc433ea2338be303fae69bbcff6a:Splice.AmuletRules:AmuletRules',
+            contractId:
+                '001e364e529d90ba28da0c99b71bf77cf464d80fc71effa25c815e7320577d212eca1212206987ff84133b0d73585fefc687c7af9bf6a31d53419ccc575be3e994f592e0cf',
+            choice: 'AmuletRules_DevNet_Tap',
+            choiceArgument: {
+                receiver:
+                    'v1-01-alice::1220a07b16cc2186d42c97242642a9db79eda4bea472963ecd42a3e057924576f573',
+                amount: '10000.0000000000',
+                openRound:
+                    '006b5fe2c819eaef2130811d27868a5fe2915dee6fa98cf1aba890543a808aba2aca121220749ca9763bfe2b5644ea0b74a27a4d85f27f33de0ae06eda17dfea6a32f52c2d',
+            },
+        })
+
+        const result = await amuletNamespace.tap(
+            'v1-01-alice::1220a07b16cc2186d42c97242642a9db79eda4bea472963ecd42a3e057924576f573',
+            '10000'
+        )
+        console.log(result)
+    })
+})
