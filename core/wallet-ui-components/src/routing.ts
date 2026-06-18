e161c955033f99525ab194545e3691d98a2a35a3
Alex Matson <alex.matson@digitalasset.com>
2026-06-18 12:16:00 -0400

    feat: support API key mgmt in frontend
    Signed-off-by: Alex Matson <alex.matson@digitalasset.com>

--- core/wallet-ui-components/src/routing.ts
+++ core/wallet-ui-components/src/routing.ts
@@ -2,6 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
 export const ALLOWED_ROUTES = [
+    '/api-keys/add',
+    '/api-keys/remove',
+    '/api-keys',
     '/login',
     '/parties/add',
     '/parties',
