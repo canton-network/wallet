37f03758b0a12dd89eef428ac5639539dcf03d85
Mateusz Piątkowski <mateusz.piatkowski@digitalasset.com>
2026-07-10 10:12:09 +0200

    feat(example-test-token-v1-registry): create dummy registry server
    Signed-off-by: Mateusz Piątkowski <mateusz.piatkowski@digitalasset.com>

---
+++ examples/test-token-v1-registry/src/sdk.ts
@@ -0,0 +1,20 @@
+// Copyright (c) 2025-2026 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import { localNetStaticConfig, SDK } from '@canton-network/wallet-sdk'
+
+const sdk = await SDK.create({
+    auth: {
+        method: 'self_signed',
+        issuer: 'unsafe-auth',
+        credentials: {
+            clientId: localNetStaticConfig.LOCALNET_USER_ID,
+            clientSecret: 'unsafe',
+            audience: 'https://canton.network.global',
+            scope: '',
+        },
+    },
+    ledgerClientUrl: localNetStaticConfig.LOCALNET_APP_USER_LEDGER_URL,
+})
+
+export default sdk
