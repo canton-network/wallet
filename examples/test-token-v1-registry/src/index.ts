facaedaf24aa84d1262aa73f2681484b3c4fcbec
Mateusz Piątkowski <mateusz.piatkowski@digitalasset.com>
2026-07-10 10:12:09 +0200

    feat(example-test-token-v1-registry): create dummy registry server
    Signed-off-by: Mateusz Piątkowski <mateusz.piatkowski@digitalasset.com>

---
+++ examples/test-token-v1-registry/src/index.ts
@@ -0,0 +1,9 @@
+// Copyright (c) 2025-2026 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import Koa from 'koa'
+import router from './router'
+
+const app = new Koa()
+
+app.use(router.routes()).use(router.allowedMethods()).listen(3000)
