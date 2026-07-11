7d7c9746e7c85a40128361fa64c0fc2b59d933af
Mateusz Piątkowski <mateusz.piatkowski@digitalasset.com>
2026-07-11 11:43:38 +0200

    refactor(wallet-sdk): export plugins, rearrange files
    Signed-off-by: Mateusz Piątkowski <mateusz.piatkowski@digitalasset.com>

--- sdk/wallet-sdk/src/plugins/index.ts
+++ sdk/wallet-sdk/src/plugins/index.ts
@@ -1,6 +1,4 @@
 // Copyright (c) 2025-2026 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-export * from './wallet/index.js'
-export * from './config.js'
-export * from './plugins/index.js'
+export * from './testToken.js'
