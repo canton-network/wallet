f874f3610f812b992e5aa67a4a50cc1fb796bdf9
Mateusz Piątkowski <mateusz.piatkowski@digitalasset.com>
2026-07-13 09:50:47 +0200

    fix: fix script
    Signed-off-by: Mateusz Piątkowski <mateusz.piatkowski@digitalasset.com>

--- scripts/src/generate-test-token.ts
+++ scripts/src/generate-test-token.ts
@@ -5,13 +5,26 @@ import * as path from 'path'
 import { getRepoRoot } from './lib/utils.js'
 import { installDPM } from './install-dpm.js'
 import { runDamlCodegen } from './lib/daml-codegen.js'
+import { existsSync } from 'fs'
+import { execSync } from 'child_process'
 
 const repoRoot = getRepoRoot()
 
 async function main() {
     await installDPM()
+
+    const localnetDir = path.join(repoRoot, '.localnet')
+    const darsDir = path.join(localnetDir, 'dars')
+
+    if (!existsSync(darsDir)) {
+        execSync('yarn script:fetch:localnet', {
+            cwd: repoRoot,
+            stdio: 'inherit',
+        })
+    }
+
     await runDamlCodegen({
-        workingDir: path.join(repoRoot, '.localnet/dars'),
+        workingDir: darsDir,
         darFileName: 'splice-test-token-v1-1.0.0.dar',
         outputDir: path.join(repoRoot, 'damljs/test-token-v1'),
     })
