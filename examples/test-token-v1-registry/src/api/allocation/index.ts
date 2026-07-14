5c8eeff41ce68f44f32f72cff2ab93c742f51cf1
Mateusz Piątkowski <mateusz.piatkowski@digitalasset.com>
2026-07-14 09:34:59 +0200

    feat(examples-test-token-v1-registry): create new api dirs
    Signed-off-by: Mateusz Piątkowski <mateusz.piatkowski@digitalasset.com>

---
+++ examples/test-token-v1-registry/src/api/allocation/index.ts
@@ -0,0 +1,13 @@
+// Copyright (c) 2025-2026 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import { OpenAPIBackend } from 'openapi-backend'
+import { availableOpenAPIPaths } from '../../common/getOpenApiPath'
+
+export const allocationAPI = new OpenAPIBackend({
+    definition: availableOpenAPIPaths['allocation-v1.yaml'],
+    quick: true,
+    handlers: {},
+})
+
+await allocationAPI.init()
