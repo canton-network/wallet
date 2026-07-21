61022dbc01011f11a8d6febfd3a6e26785eccecc
rukmini-basu-da <126689545+rukmini-basu-da@users.noreply.github.com>
2026-07-21 17:55:36 -0400

    fix: add pagination to list tx (#2166)
    * pagination
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * test
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * test
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * store sql test
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * test
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * test
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * fix types
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * update user api controller
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * test
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * test
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * add pagination to store internal
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * test
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * use pagination in activities
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * refactor
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * refactor
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * fix computed total calculation
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * test
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * add total count
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * add test
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * rename variables
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * fix test
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * add test
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * add tests for stores
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    ---------
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    Signed-off-by: Mateusz Piątkowski <mateusz.piatkowski@digitalasset.com>

--- core/wallet-store-sql/src/store-sql.ts
+++ core/wallet-store-sql/src/store-sql.ts
@@ -51,7 +51,6 @@ import {
 } from './schema.js'
 import pg from 'pg'
 import { sql } from 'kysely'
-import { AccessToken } from '@canton-network/core-types'
 
 export class StoreSql implements BaseStore, AuthAware<StoreSql> {
     authContext: AuthContext | undefined
