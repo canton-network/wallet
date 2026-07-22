7cc19e9f55fc0720a27b4732c587dbfc70139db6
Mateusz Piątkowski <mateusz.piatkowski@digitalasset.com>
2026-07-22 15:08:27 +0200

    feat(example-test-token-v1-registry): adjust after merge
    Signed-off-by: Mateusz Piątkowski <mateusz.piatkowski@digitalasset.com>

--- examples/test-token-v1-registry/tsdown.config.ts
+++ examples/test-token-v1-registry/tsdown.config.ts
@@ -1,15 +1,13 @@
 // Copyright (c) 2025-2026 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { defineConfig } from 'tsup'
-import { base } from '../../tsup.base'
+import { defineConfig } from 'tsdown'
+import { base } from '../../tsdown.base.ts'
 
 export default defineConfig({
     ...base,
+    format: ['esm'],
+    platform: 'node',
     entry: ['src/index.ts'],
-
-    // Overrides to support top-level await
     target: 'es2022',
-    outExtension: () => ({ js: '.js' }),
-    format: 'esm',
 })
