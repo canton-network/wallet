9950f44fc176197f4ed4a7b1663df52eb7a1918e
Mateusz Piątkowski <mateusz.piatkowski@digitalasset.com>
2026-07-23 19:24:16 +0200

    docs(example-test-token-v1-registry): add explanatory comments
    Signed-off-by: Mateusz Piątkowski <mateusz.piatkowski@digitalasset.com>

--- examples/test-token-v1-registry/src/types.ts
+++ examples/test-token-v1-registry/src/types.ts
@@ -1,6 +1,9 @@
 // Copyright (c) 2025-2026 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+/**
+ * An arbitrary structure identifying a common interface for all method handlers.
+ */
 export type APIHandlerResponse<Payload = unknown> = {
     status?: number // default is 200
     payload: Payload
@@ -11,6 +14,9 @@ type ExtractOperationPayload<OperationReturn> =
         ? NonNullable<ResponseBody>
         : Awaited<OperationReturn>
 
+/**
+ * Generic type that is used for each API handler. Uses generated interfaces to ensure I/O strutures requirements are met.
+ */
 export type APIOperationHandler<OperationHandler> = OperationHandler extends (
     ...args: infer Args
 ) => infer OperationReturn
