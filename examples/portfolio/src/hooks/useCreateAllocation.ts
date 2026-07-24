d448e09e8c63e6859d9d5ae5065e366572cb955a
Fayi <112705750+fayi-da@users.noreply.github.com>
2026-07-24 13:14:49 +0100

    feat: migrate portfolio allocations to the wallet-sdk (#2192)
    Signed-off-by: Fayi Femi-Balogun <fayimora.femibalogun@digitalasset.com>

--- examples/portfolio/src/hooks/useCreateAllocation.ts
+++ examples/portfolio/src/hooks/useCreateAllocation.ts
@@ -4,24 +4,44 @@
 import { useQueryClient, useMutation } from '@tanstack/react-query'
 import { type AllocationSpecification } from '@canton-network/core-token-standard'
 import { type PartyId } from '@canton-network/core-types'
-import { usePortfolio } from '../contexts/PortfolioContext'
-import { useRegistryUrls } from './useRegistryUrls'
+import { submitViaProvider } from '@lib/submit'
+import { resolveAllocationAsset } from '@utils/allocation'
+import { useInstruments } from './useInstruments'
+import { useReachableRegistryUrls, useRegistryUrls } from './useRegistryUrls'
+import { useWalletSdk } from './useWalletSdk'
 import { queryKeys } from './query-keys'
 
 export const useCreateAllocation = () => {
-    const { createAllocation } = usePortfolio()
+    const { sdk } = useWalletSdk()
     const registryUrls = useRegistryUrls()
+    const { reachableRegistryUrls } = useReachableRegistryUrls()
+    const instruments = useInstruments()
     const queryClient = useQueryClient()
 
     return useMutation({
-        mutationFn: (args: {
+        mutationFn: async (args: {
             party: PartyId
             allocationSpecification: AllocationSpecification
-        }) =>
-            createAllocation({
+        }) => {
+            const asset = resolveAllocationAsset({
+                instrumentId:
+                    args.allocationSpecification.transferLeg.instrumentId,
+                instruments,
                 registryUrls,
-                ...args,
-            }),
+                reachableRegistryUrls,
+            })
+            if (!sdk) {
+                throw new Error('Wallet SDK is not ready')
+            }
+
+            const preparedCommand =
+                await sdk.token.allocation.instruction.create({
+                    allocationSpecification: args.allocationSpecification,
+                    asset,
+                })
+
+            await submitViaProvider(preparedCommand, args.party)
+        },
         onSuccess: async (_, args) => {
             await Promise.all([
                 queryClient.invalidateQueries({
