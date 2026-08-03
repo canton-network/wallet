ea906f89ff12b8eb5416504c177f2e9042001394
Mateusz Piątkowski <mateusz.piatkowski@digitalasset.com>
2026-08-03 11:42:27 +0200

    fix(example-portfolio): fix build
    Signed-off-by: Mateusz Piątkowski <mateusz.piatkowski@digitalasset.com>

--- examples/portfolio/src/types/instruments.ts
+++ examples/portfolio/src/types/instruments.ts
@@ -1,11 +1,12 @@
 // Copyright (c) 2025-2026 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { type metadataRegistryTypes } from '@canton-network/core-token-standard'
+import type { OffLedger } from '@canton-network/core-token-standard'
 import { type PartyId } from '@canton-network/core-types'
 import type { AssetBody } from '@canton-network/wallet-sdk'
 
-export type Instrument = metadataRegistryTypes['schemas']['Instrument']
+export type Instrument =
+    OffLedger.MetadataV1.components['schemas']['Instrument']
 
 export type PortfolioInstrument = AssetBody & {
     name: string
