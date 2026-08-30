1e1cb02bf5057f272eb97283e8c97318ae61ea1f
singhyash05 <yashsingh5609@gmail.com>
2026-08-30 08:14:26 +0530

    feat: default wallet picker to in-page modal
    Ship the production wallet picker as an in-page modal (SDK default) while keeping the popup path via setWalletPicker(pickWallet). Wire dapp-sdk + WalletConnect QR for the modal, add unit/e2e coverage, adapt ping disconnect status + extension helpers, and point WG test ledger URLs at localnet 2975.
    
    Signed-off-by: singhyash05 <yashsingh5609@gmail.com>

--- core/wallet-ui-components/src/index.ts
+++ core/wallet-ui-components/src/index.ts
@@ -32,6 +32,7 @@ export * from './components/transaction-card.js'
 export * from './components/transaction-detail.js'
 
 export * from './windows/wallet-picker.js'
+export * from './windows/wallet-picker-modal.js'
 export * from './windows/popup.js'
 
 export * from './handle-errors.js'
