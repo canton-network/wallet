b543a8da89c19cc0ef0ebb62df88370b6d4e0238
Pawel Stepien <pawel.stepien@digitalasset.com>
2026-09-09 13:50:58 +0200

    canton 3.6 api specs
    Signed-off-by: Pawel Stepien <pawel.stepien@digitalasset.com>

--- core/ledger-client-types/src/index.ts
+++ core/ledger-client-types/src/index.ts
@@ -3,9 +3,13 @@
 
 export * as v3_5 from './generated-clients/openapi-3.5.10.js'
 export * as v3_5_async from './generated-clients/asyncapi-3.5.10.js'
+export * as v3_6 from './generated-clients/openapi-3.6.0.js'
+export * as v3_6_async from './generated-clients/asyncapi-3.6.0.js'
 import * as V3_5_provider from './generated-clients/openapi-3.5.10-provider-types.js'
 import * as openapi_v3_5 from './generated-clients/openapi-3.5.10.js'
+import * as openapi_v3_6 from './generated-clients/openapi-3.6.0.js'
 import * as asyncapi_v3_5 from './generated-clients/asyncapi-3.5.10.js'
+import * as asyncapi_v3_6 from './generated-clients/asyncapi-3.6.0.js'
 import {
     getPaths as getPaths_v3_5,
     postPaths as postPaths_v3_5,
@@ -13,31 +17,46 @@ import {
 
 export * from './utils.js'
 
-export const supportedLedgerApiVersions = ['3.5'] as const
+export const supportedLedgerApiVersions = ['3.5', '3.6'] as const
 export type LedgerApiVersion = (typeof supportedLedgerApiVersions)[number]
 
-export const supportedAsyncApiVersions = ['3.5'] as const
+export const supportedAsyncApiVersions = ['3.5', '3.6'] as const
 export type AsyncApiVersion = (typeof supportedAsyncApiVersions)[number]
 
 export const ledgerApiByVersion = {
     '3.5': openapi_v3_5,
+    '3.6': openapi_v3_6,
 } as const
 
 export const asyncApiByVersion = {
     '3.5': asyncapi_v3_5,
+    '3.6': asyncapi_v3_6,
 } as const
 
 export type LedgerSchemasByVersion = {
     '3.5': openapi_v3_5.components['schemas']
+    '3.6': openapi_v3_6.components['schemas']
 }
 
 export type LedgerPathsByVersion = {
     '3.5': openapi_v3_5.paths
+    '3.6': openapi_v3_6.paths
 }
 
-export type LedgerCommonSchemas = LedgerSchemasByVersion['3.5']
+// type CommonKeys<A, B> = keyof A & keyof B
+type Common<A, B> = {
+    [K in keyof A & keyof B]: A[K] & B[K]
+}
 
-export type LedgerCommonPaths = LedgerPathsByVersion['3.5']
+export type LedgerCommonSchemas = Common<
+    LedgerSchemasByVersion['3.5'],
+    LedgerSchemasByVersion['3.6']
+>
+
+export type LedgerCommonPaths = Common<
+    LedgerPathsByVersion['3.5'],
+    LedgerPathsByVersion['3.6']
+>
 
 export type AsyncChannelsByVersion = {
     '3.5': typeof asyncapi_v3_5.CHANNELS
