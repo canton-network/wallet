0b9584cdb4c66f72a77781c9fa0a1677099cfbce
Pawel Stepien <pawel.stepien@digitalasset.com>
2026-09-10 12:18:07 +0200

    missing rename
    Signed-off-by: Pawel Stepien <pawel.stepien@digitalasset.com>

--- sdk/wallet-sdk/src/wallet/sdk.ts
+++ sdk/wallet-sdk/src/wallet/sdk.ts
@@ -25,7 +25,7 @@ import {
     OfflineInitializedSDK,
 } from './init/initializedSDK.js'
 import {
-    LedgerTypes as LedgerRpc,
+    LedgerApiOperations as LedgerRpc,
     type LedgerCommonSchemas,
 } from '@canton-network/core-ledger-client-types'
 import { AllowedLogAdapters } from './logger/types.js'
@@ -48,7 +48,7 @@ export {
     signTransactionHash,
     getPublicKeyFromPrivate,
 } from '@canton-network/core-signing-lib'
-export type LedgerTypes = LedgerCommonSchemas
+export type { LedgerCommonSchemas }
 
 export * from './init/index.js'
 export type {
