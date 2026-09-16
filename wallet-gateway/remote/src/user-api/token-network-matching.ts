ecb3d17b601e8c98edb8153c2f933fc169d5416e
Marc Juchli <120378272+mjuchli-da@users.noreply.github.com>
2026-09-16 15:31:07 +0200

    fix: revert #2445 (#2451)
    Signed-off-by: Marc Juchli <marc.juchli@digitalasset.com>

--- wallet-gateway/remote/src/user-api/token-network-matching.ts
+++ wallet-gateway/remote/src/user-api/token-network-matching.ts
@@ -36,6 +36,14 @@ export function assertTokenClaimsMatchNetwork(
         )
     }
 
+    // check client ID based on `azp` (Authorized Party) claim or `client_id` claim, only if present.
+    const tokenClientId = tokenClaims.azp || tokenClaims.client_id
+    if (tokenClientId && tokenClientId !== network.auth.clientId) {
+        throw new Error(
+            `Token client ID doesn't match network's auth clientId.`
+        )
+    }
+
     if (idp.type === 'self_signed') {
         const { kid } = decodeProtectedHeader(accessToken)
         if (!kid) {
