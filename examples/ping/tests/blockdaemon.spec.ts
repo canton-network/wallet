c645d33a29e87b5d87b969f6fee5fef7b6f1139f
rukmini-basu-da <126689545+rukmini-basu-da@users.noreply.github.com>
2026-09-18 11:58:37 -0400

    fix: async approval add polling (#2381)
    * async approval
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * fix conflicts
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * tx service changes
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * fix tests
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * fix tests
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * e2e test
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * emit status for dapp
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * fix
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * test e2e
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * test
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * update lock
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * add down
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * add migration test
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * add test for pending
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * migrate down test
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * cleanup
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * fix
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * handle applySigningResult value
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * update
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * update
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * add check for blockdaemon email in auth context
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * test
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * tx service test update
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * remove unused params
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * add extended retry
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * test
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * add back isSigning
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * remove todo
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * update lock
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    ---------
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>

--- examples/ping/tests/blockdaemon.spec.ts
+++ examples/ping/tests/blockdaemon.spec.ts
@@ -57,13 +57,14 @@ test.describe('Blockdaemon external signing', () => {
             submission.externalTxId,
             'signed'
         )
-        await wg.executeSignedTransaction({ waitForClose: false })
 
         await expectTxStatusInDappEvents(
             dappPage,
             submission.commandId,
             'signed'
         )
+        await wg.executeSignedTransaction({ waitForClose: false })
+
         await expectTxStatusInDappEvents(
             dappPage,
             submission.commandId,
@@ -90,7 +91,6 @@ test.describe('Blockdaemon external signing', () => {
             submission.externalTxId,
             'rejected'
         )
-        await wg.executeSignedTransaction({ waitForClose: false })
 
         await expectTxStatusInDappEvents(
             dappPage,
@@ -109,7 +109,6 @@ test.describe('Blockdaemon external signing', () => {
             submission.externalTxId,
             'failed'
         )
-        await wg.executeSignedTransaction({ waitForClose: false })
 
         await expectTxStatusInDappEvents(
             dappPage,
--- wallet-gateway/remote/src/user-api/controller.test.ts
+++ wallet-gateway/remote/src/user-api/controller.test.ts
@@ -995,6 +995,7 @@ describe('userController', () => {
                 expect.objectContaining({
                     getWithRetry: ledgerMocks.getWithRetry,
                 }),
+                auth,
                 expect.objectContaining({ id: storeNetwork.id })
             )
             expect(result).toEqual({ commandId: pendingTransaction.commandId })
@@ -1028,6 +1029,7 @@ describe('userController', () => {
                 expect.objectContaining({
                     getWithRetry: ledgerMocks.getWithRetry,
                 }),
+                auth,
                 expect.objectContaining({ id: storeNetwork.id })
             )
             expect(result).toEqual({ commandId: pendingTransaction.commandId })
