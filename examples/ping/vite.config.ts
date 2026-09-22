e62e59b2ade85c4078a1c44e8d3bc06749bd00d3
rukmini-basu-da <126689545+rukmini-basu-da@users.noreply.github.com>
2026-09-22 16:34:31 -0400

    fix: fix ping e2e (#2489)
    * fix ping e2e for now
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    * actual fix
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>
    
    ---------
    
    Signed-off-by: rukmini-basu-da <rukmini.basu@digitalasset.com>

--- examples/ping/vite.config.ts
+++ examples/ping/vite.config.ts
@@ -7,6 +7,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    build: {
+        commonjsOptions: {
+            include: [/node_modules/],
+            transformMixedEsModules: true,
+        },
+    },
     server: {
         port: 8080,
     },
