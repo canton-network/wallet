2e80ee8a16ef2c668afadb81dda76e1c27dc52eb
Pawel Stepien <pawel.stepien@digitalasset.com>
2026-09-24 13:08:24 +0200

    get adminAuth token from another idp
    Signed-off-by: Pawel Stepien <pawel.stepien@digitalasset.com>

--- wallet-gateway/remote/src/ledger/self-issued-onboarding.ts
+++ wallet-gateway/remote/src/ledger/self-issued-onboarding.ts
@@ -5,13 +5,14 @@ import { LedgerClient } from '@canton-network/core-ledger-client'
 import {
     type AuthAware,
     AuthTokenProvider,
+    resolveAuthIdentityProviderId,
 } from '@canton-network/core-wallet-auth'
 import type { Store } from '@canton-network/core-wallet-store'
 import type { Logger } from 'pino'
-import type { SigningDrivers } from '../signing/signing-drivers.js'
 import { PartyAllocationService } from './party-allocation-service.js'
 import { SelfIssuedTokenService } from './self-issued-token-service.js'
 import { WalletAllocationService } from './wallet-allocation/wallet-allocation-service.js'
+import { SigningDrivers } from '@canton-network/core-wallet-services'
 
 // TODO move it somewhere better
 export async function createSelfIssuedOnboardingService(
@@ -34,15 +35,11 @@ export async function createSelfIssuedOnboardingService(
     if (network.id !== networkId) {
         throw new Error('Onboarding session network does not match networkId')
     }
-    if ((network.auth as { method: string }).method !== 'self_issued') {
+    if (network.auth.method !== 'self_issued') {
         throw new Error('Network does not use self_issued authentication')
     }
     const idp = await bootstrapStore.getIdp(network.identityProviderId)
-    if (
-        // eslint-disable-next-line no-constant-condition,no-constant-binary-expression
-        false && // TODO temp
-        (idp as { type: string }).type !== 'self_issued'
-    ) {
+    if (idp.type !== 'self_issued') {
         throw new Error(
             'Identity provider is not configured for self_issued authentication'
         )
@@ -51,8 +48,15 @@ export async function createSelfIssuedOnboardingService(
         throw new Error('No admin auth configured')
     }
 
+    const adminIdp = await bootstrapStore.getIdp(
+        resolveAuthIdentityProviderId(
+            network.adminAuth,
+            network.identityProviderId
+        )
+    )
+    // TODO should I actually get adminAuth.idp inside token provider?
     const adminTokenProvider = AuthTokenProvider.fromGatewayConfig(
-        idp,
+        adminIdp,
         network.adminAuth,
         logger
     )
